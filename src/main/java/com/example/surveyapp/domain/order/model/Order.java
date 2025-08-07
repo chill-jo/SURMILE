07ac035e5651746914a31debc875a98898be28a3
handokei <ehgks994@naver.com>
2025-08-07 14:35:43 +0900

    refactor: Order 에그리거트 루트 설정, OrderNumber VO 설정
    - 값만 가지고 있으면 되는 OrderNumber VO타입으로 변경

--- src/main/java/com/example/surveyapp/domain/order/model/Order.java
+++ src/main/java/com/example/surveyapp/domain/order/model/Order.java
@@ -1,14 +1,11 @@
 package com.example.surveyapp.domain.order.model;
 
-import com.example.surveyapp.domain.product.domain.model.Product;
-import com.example.surveyapp.domain.user.domain.model.User;
 import com.example.surveyapp.global.config.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.UUID;
 
 @Entity
 @Table(name = "orders")
@@ -19,13 +16,13 @@ public class Order extends BaseEntity {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "order_number", nullable = false, unique = true, length = 36)
-    private String orderNumber;
+    @Embedded
+    private OrderNumber orderNumber;
 
    @JoinColumn(name = "user_id", nullable = false)
     private Long userId;
 
-  @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id", nullable = false)
     private Long productId;
 
     @Column(nullable = false)
@@ -42,7 +39,7 @@ public class Order extends BaseEntity {
     }
 
     @Builder(access = AccessLevel.PRIVATE)
-    private Order(String orderNumber, Long userId, Long productId, String title, Long price) {
+    private Order(OrderNumber orderNumber, Long userId, Long productId, String title, Long price) {
         this.orderNumber = orderNumber;
         this.userId = userId;
         this.productId = productId;
@@ -52,10 +49,9 @@ public class Order extends BaseEntity {
 
     public static Order create(Long userId, Long productId, String title, Long price) {
 
-        String orderNumber = orderNumberGenerator();
         return Order.builder()
                 .userId(userId)
-                .orderNumber(orderNumber)
+                .orderNumber(OrderNumber.generator())
                 .productId(productId)
                 .title(title)
                 .price(price)
@@ -63,10 +59,5 @@ public class Order extends BaseEntity {
     }
 
 
-    //UUID 날짜 + 랜덤 숫자 8개를 만들기 위한 generator
-    private static String orderNumberGenerator() {
-        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-        int randoNum = (int) ((Math.random() * 90000000) + 10000000); //8자리 랜덤 숫자 만들기
-        return date + randoNum;
-    }
+
 }
