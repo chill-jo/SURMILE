eb2366d10d93eec2651a94e1cfe6146e7d017c18
An-jihyun <anjh322@gmail.com>
2025-08-07 20:09:03 +0900

    ref: AccessDeniedHandler에 ObjectMapper 주입 방식 적용

--- src/main/java/com/example/surveyapp/global/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/surveyapp/global/security/handler/CustomAccessDeniedHandler.java
@@ -6,15 +6,20 @@ import com.example.surveyapp.global.response.exception.ErrorResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import java.io.IOException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
+@RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
@@ -25,7 +30,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     private void writeErrorResponse(HttpServletResponse response, ErrorResponseDto error) throws IOException {
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        new ObjectMapper().writeValue(response.getWriter(), ApiResponse.fail(error.getMessage(), error));
+        objectMapper.writeValue(response.getWriter(), ApiResponse.fail(error.getMessage(), error));
     }
 }
 
