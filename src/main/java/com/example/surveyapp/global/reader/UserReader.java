e141cc49d47cea57ae5b1ccd929d7b795a2ea361
kimminsung <shiwing15@naver.com>
2025-08-08 12:28:44 +0900

    feat: userReader 수정

--- src/main/java/com/example/surveyapp/global/reader/UserReader.java
+++ src/main/java/com/example/surveyapp/global/reader/UserReader.java
@@ -1,9 +1,8 @@
 package com.example.surveyapp.global.reader;
 
-import com.example.surveyapp.domain.user.domain.model.User;
 import com.example.surveyapp.domain.user.domain.model.UserRoleEnum;
 
 public interface UserReader {
     void validateUserIdOrThrow(Long userId);
-    boolean isSurveyor(Long userId);
+    boolean validateUserRole(Long userId, UserRoleEnum userRole);
 }
