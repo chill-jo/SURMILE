57300901d925a5de30245acaf6c9ec32900d4fb0
gsemily <gsemily22@gmail.com>
2025-08-08 15:11:57 +0900

    feat: chat DTO 추가

---
+++ src/main/java/com/example/surveyapp/domain/ai/chat/controller/dto/ChatResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.surveyapp.domain.ai.chat.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatResponseDto {
+    private String answer;
+}
