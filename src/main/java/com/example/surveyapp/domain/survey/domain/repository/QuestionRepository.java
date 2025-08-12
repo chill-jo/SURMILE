cd011aaacf269cfeaffd816dc4686d41b953053a
kimminsung <shiwing15@naver.com>
2025-08-12 19:51:24 +0900

    refactor: 사용하지 않는 메서드, import 삭제

--- src/main/java/com/example/surveyapp/domain/survey/domain/repository/QuestionRepository.java
+++ src/main/java/com/example/surveyapp/domain/survey/domain/repository/QuestionRepository.java
@@ -1,19 +1,14 @@
 package com.example.surveyapp.domain.survey.domain.repository;
 
-import com.example.surveyapp.domain.survey.domain.model.entity.Question;
-import com.example.surveyapp.domain.survey.domain.model.entity.Survey;
-import com.example.surveyapp.domain.survey.infra.QuestionReadEntity;
+import com.example.surveyapp.domain.survey.infrastructure.QuestionReadEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionReadEntity, Long> {
 
-    Page<QuestionReadEntity> findAllBySurveyId(@Param("surveyId") Long surveyId, Pageable pageable);
+    //Page<QuestionReadEntity> findAllBySurveyId(@Param("surveyId") Long surveyId, Pageable pageable);
 }
