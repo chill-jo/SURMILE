32537633876122bab7559ad126ac27dca028709a
kimminsung <50131278+urzn@users.noreply.github.com>
2025-08-12 22:45:43 +0900

    Issues/286 SurveyAnswer 리팩토링 (#287)
    * feat: Survey 참여인원 마감 event 생성
    
    * feat: Survey 도메인 조회를 위한 SurveyFacade 생성
    
    * feat: Survey 도메인에서 참여 검증을 위한 SurveyAnswerFacade 구현체 생성

---
+++ src/main/java/com/example/surveyapp/domain/surveyanswer/application/facade/SurveyFacade.java
@@ -0,0 +1,18 @@
+package com.example.surveyapp.domain.surveyanswer.application.facade;
+
+import com.example.surveyapp.domain.survey.application.dto.OptionDto;
+import com.example.surveyapp.domain.survey.application.dto.QuestionIdAndTypeDto;
+import com.example.surveyapp.domain.survey.application.dto.QuestionDto;
+import com.example.surveyapp.domain.survey.application.dto.SurveyInfoDto;
+
+import java.util.List;
+
+public interface SurveyFacade {
+    void validateSurveyStartable(Long surveyId);
+    QuestionIdAndTypeDto getQuestionIdAndTypeByNumber(Long surveyId, Long number);
+    Long getPointPerPersonBySurveyId(Long surveyId);
+    SurveyInfoDto getSurveyInfo(Long surveyId);
+    List<QuestionDto> getQuestionDtos(Long surveyId);
+    List<OptionDto> getOptionDtos(Long surveyId, Long questionId);
+
+}
