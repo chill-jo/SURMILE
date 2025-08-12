2bcfea6dd98b38f31790af03bc2e785612ac2d89
kimminsung <shiwing15@naver.com>
2025-08-12 21:53:19 +0900

    refactor: 사용하지 않는 import 삭제, 불필요한 공백 삭제

--- src/main/java/com/example/surveyapp/domain/surveyanswer/domain/repository/SurveyOptionsAnswerRepository.java
+++ src/main/java/com/example/surveyapp/domain/surveyanswer/domain/repository/SurveyOptionsAnswerRepository.java
@@ -1,9 +1,8 @@
 package com.example.surveyapp.domain.surveyanswer.domain.repository;
 
-import com.example.surveyapp.domain.survey.domain.model.entity.Question;
 import com.example.surveyapp.domain.surveyanswer.domain.model.SurveyOptionsAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SurveyOptionsAnswerRepository extends JpaRepository<SurveyOptionsAnswer, Long> {
-    public Long countByQuestionAndNumber(Question question, Long number);
+    Long countByQuestionIdAndNumber(Long questionId, Long number);
 }
