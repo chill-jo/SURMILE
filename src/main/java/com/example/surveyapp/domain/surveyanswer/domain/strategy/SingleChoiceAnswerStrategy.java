4e68b0a94e0b989f108c6fae91a86305568f00ee
kimminsung <shiwing15@naver.com>
2025-08-12 21:54:48 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/example/surveyapp/domain/surveyanswer/domain/strategy/SingleChoiceAnswerStrategy.java
+++ src/main/java/com/example/surveyapp/domain/surveyanswer/domain/strategy/SingleChoiceAnswerStrategy.java
@@ -1,7 +1,6 @@
 package com.example.surveyapp.domain.surveyanswer.domain.strategy;
 
-import com.example.surveyapp.domain.surveyanswer.controller.dto.request.QuestionAnswerRequestDto;
-import com.example.surveyapp.domain.survey.domain.model.entity.Question;
+import com.example.surveyapp.domain.surveyanswer.presentation.dto.request.QuestionAnswerRequestDto;
 import com.example.surveyapp.domain.surveyanswer.domain.model.SurveyAnswer;
 import com.example.surveyapp.domain.surveyanswer.domain.model.SurveyOptionsAnswer;
 import com.example.surveyapp.domain.survey.domain.model.enums.QuestionType;
