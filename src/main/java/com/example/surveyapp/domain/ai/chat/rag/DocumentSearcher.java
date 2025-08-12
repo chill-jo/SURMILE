bdcf8d167e22fa3c691e85f1bf757dc8301445b4
gsemily <120404242+gsemily@users.noreply.github.com>
2025-08-12 11:50:03 +0900

    feat: spring AI 기반 챗봇 구현 (#263)
    * refactor: ai.config 리팩토링
    
    * feat: chat DTO 추가
    
    * build: gradle 의존성 추가, redis 설정 추가
    
    - redis vector store 의존성 추가
    - application.yml 파일에 redis 및 redis vector store 추가
    - docker-compose 파일에 redis 설정 추가
    
    * chore: redis 포트 제거
    
    * feat: DocumentIndexer 추가
    
    * feat: DocumentIndexer, DocumentSearcher 추가
    
    * feat: ChatService 추가, ChatRequestDto 변수명 수정
    
    * feat: ChatController 생성
    
    * feat: initial DocumentLoader 추가
    
    * feat: 문서 입력 기능 추가
    
    * feat: EmbeddingModelConfig 추가

---
+++ src/main/java/com/example/surveyapp/domain/ai/chat/rag/DocumentSearcher.java
@@ -0,0 +1,25 @@
+package com.example.surveyapp.domain.ai.chat.rag;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class DocumentSearcher {
+    private final VectorStore vectorStore;
+
+    public List<Document> search(String query, int topK, double similarityThreshold) {
+        return vectorStore.similaritySearch(
+                SearchRequest.builder()
+                        .query(query)
+                        .topK(topK)
+                        .similarityThreshold(similarityThreshold)
+                        .build()
+        );
+    }
+}
--- src/main/java/com/example/surveyapp/domain/ai/config/ChatClientConfig.java
+++ src/main/java/com/example/surveyapp/domain/ai/config/ChatClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.surveyapp.domain.ai.moderation.config;
+package com.example.surveyapp.domain.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
