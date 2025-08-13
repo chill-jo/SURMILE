b8a524e6261efcffed775c2fd037b2deac1a048b
kimminsung <shiwing15@naver.com>
2025-08-13 16:22:49 +0900

    refactor: 직렬화 오류를 위해 NoArgsConstructor 추가

--- src/main/java/com/example/surveyapp/domain/survey/presentation/dto/request/QuestionCreateRequestDto.java
+++ src/main/java/com/example/surveyapp/domain/survey/presentation/dto/request/QuestionCreateRequestDto.java
@@ -3,6 +3,7 @@ package com.example.surveyapp.domain.survey.presentation.dto.request;
 import com.example.surveyapp.domain.survey.domain.model.enums.QuestionType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +11,17 @@ import org.hibernate.validator.constraints.Length;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QuestionCreateRequestDto {
 
     @NotNull(message = "질문 번호는 필수입니다.")
-    private final Long number;
+    private Long number;
 
     @NotBlank(message = "질문 내용은 필수입니다.")
     @Length(min = 5, max = 255, message = "질문 내용은 5~255자 사이여야 합니다.")
-    private final String content;
+    private String content;
 
     @NotNull(message = "질문 유형은 필수입니다.")
-    private final QuestionType type;
+    private QuestionType type;
 
 }
