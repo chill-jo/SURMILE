0eecae2ef6bd8f3b7717fa4cd05fa4a4deb88f09
kimminsung <shiwing15@naver.com>
2025-08-15 18:28:44 +0900

    test: OptionsControllerTest RestDoc 문서화 추가

--- src/test/java/com/example/surveyapp/domain/survey/presentation/OptionsControllerTest.java
+++ src/test/java/com/example/surveyapp/domain/survey/presentation/OptionsControllerTest.java
@@ -4,7 +4,9 @@ import com.example.surveyapp.domain.survey.application.OptionsService;
 import com.example.surveyapp.domain.survey.application.QuestionService;
 import com.example.surveyapp.domain.survey.domain.model.enums.QuestionType;
 import com.example.surveyapp.domain.survey.presentation.dto.request.OptionCreateRequestDto;
+import com.example.surveyapp.domain.survey.presentation.dto.request.OptionUpdateRequestDto;
 import com.example.surveyapp.domain.survey.presentation.dto.request.QuestionCreateRequestDto;
+import com.example.surveyapp.domain.survey.presentation.dto.request.QuestionUpdateRequestDto;
 import com.example.surveyapp.domain.survey.presentation.dto.response.OptionResponseDto;
 import com.example.surveyapp.domain.survey.presentation.dto.response.PageQuestionResponseDto;
 import com.example.surveyapp.domain.survey.presentation.dto.response.QuestionResponseDto;
@@ -22,6 +24,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
@@ -37,6 +40,14 @@ import static org.mockito.Mockito.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -81,16 +92,43 @@ public class OptionsControllerTest {
                 .thenReturn(responseDto);
 
         ResultActions actions = mockMvc.perform(post("/api/survey/{surveyId}/question/{questionId}/option", surveyId, questionId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {jwt_token}")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(requestDto)));
 
         verify(optionsService, times(1))
                 .createOption(eq(userId), eq(surveyId), eq(questionId), any(OptionCreateRequestDto.class));
 
-        actions.andDo(print())
-                .andExpect(status().isCreated())
+        actions.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.data.number").value(requestDto.getNumber()))
-                .andExpect(jsonPath("$.data.content").value(requestDto.getContent()));
+                .andExpect(jsonPath("$.data.content").value(requestDto.getContent()))
+                .andDo(document("create-option",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("JWT 인증 토큰 (Bearer + 토큰 값)")
+                                        .attributes(key("format").value("Bearer {jwt_token"))
+                        ),
+                        pathParameters(
+                                parameterWithName("surveyId").description("설문 ID"),
+                                parameterWithName("questionId").description("질문 ID")
+                        ),
+                        requestFields(
+                                fieldWithPath("number").type(JsonFieldType.NUMBER).description("선택지 번호"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("선택지 내용")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("생성된 선택지 위치")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("요청 성공 여부"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청 결과 메시지"),
+                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("타임스탬프"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("선택지 ID"),
+                                fieldWithPath("data.number").type(JsonFieldType.NUMBER).description("선택지 번호"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("선택지 내용")
+                        )
+                ))
+        ;
     }
 
     @Test
@@ -116,17 +154,96 @@ public class OptionsControllerTest {
 
         when(optionsService.getOptions(userId, surveyId, questionId)).thenReturn(dtoList);
 
-        ResultActions actions = mockMvc.perform(get("/api/survey/{surveyId}/question/{questionId}/option", surveyId, questionId));
+        ResultActions actions = mockMvc.perform(get("/api/survey/{surveyId}/question/{questionId}/option", surveyId, questionId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {jwt_token}"));
 
         verify(optionsService, times(1))
                 .getOptions(userId, surveyId, questionId);
 
-        actions.andDo(print())
-                .andExpect(status().isOk())
+        actions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.data[0].id").value(1L))
                 .andExpect(jsonPath("$.data[1].id").value(2L))
                 .andExpect(jsonPath("$.data[0].content").value("테스트선택지내용1"))
-                .andExpect(jsonPath("$.data[1].content").value("테스트선택지내용2"));
+                .andExpect(jsonPath("$.data[1].content").value("테스트선택지내용2"))
+                .andDo(document("get-options",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("JWT 인증 토큰 (Bearer + 토큰 값)")
+                                        .attributes(key("format").value("Bearer {jwt_token"))
+                        ),
+                        pathParameters(
+                                parameterWithName("surveyId").description("설문 ID"),
+                                parameterWithName("questionId").description("질문 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("요청 성공 여부"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청 결과 메시지"),
+                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("타임스탬프"),
+                                fieldWithPath("data[].id").type(JsonFieldType.NUMBER).description("선택지 ID"),
+                                fieldWithPath("data[].number").type(JsonFieldType.NUMBER).description("선택지 번호"),
+                                fieldWithPath("data[].content").type(JsonFieldType.STRING).description("선택지 내용")
+                        )
+                ))
+        ;
+    }
+
+    @Test
+    @DisplayName("기능_선택지_상세정보를_수정한다")
+    @WithCustomMockUser(id = 1, role = UserRoleEnum.SURVEYOR)
+    void 선택지_상세정보를_수정한다() throws Exception{
+        Long surveyId = 1L;
+        Long questionId = 1L;
+        Long optionId = 1L;
+        Long userId = 1L;
+        OptionUpdateRequestDto requestDto = new OptionUpdateRequestDto(
+                1L,
+                "테스트선택지내용수정"
+        );
+
+        OptionResponseDto responseDto = new OptionResponseDto(
+                1L,
+                1L,
+                "테스트선택지내용수정"
+        );
+
+        when(optionsService.updateOption(eq(userId), eq(surveyId), eq(questionId), eq(optionId), any(OptionUpdateRequestDto.class)))
+                .thenReturn(responseDto);
+
+        ResultActions actions = mockMvc.perform(patch("/api/survey/{surveyId}/question/{questionId}/option/{optionId}", surveyId, questionId, optionId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {jwt_token}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestDto)));
+
+        verify(optionsService, times(1))
+                .updateOption(eq(userId), eq(surveyId), eq(questionId), eq(optionId), any(OptionUpdateRequestDto.class));
+
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.content").value(requestDto.getContent()))
+                .andDo(document("update-option",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("JWT 인증 토큰 (Bearer + 토큰 값)")
+                                        .attributes(key("format").value("Bearer {jwt_token"))
+                        ),
+                        pathParameters(
+                                parameterWithName("surveyId").description("설문 ID"),
+                                parameterWithName("questionId").description("질문 ID"),
+                                parameterWithName("optionId").description("선택지 ID")
+                        ),
+                        requestFields(
+                                fieldWithPath("number").type(JsonFieldType.NUMBER).description("질문 번호"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("질문 내용")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("요청 성공 여부"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청 결과 메시지"),
+                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("타임스탬프"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("선택지 ID"),
+                                fieldWithPath("data.number").type(JsonFieldType.NUMBER).description("선택지 번호"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("선택지 내용")
+                        )
+                ))
+        ;
     }
     @Test
     @DisplayName("기능_선택지를_삭제한다")
@@ -139,12 +256,32 @@ public class OptionsControllerTest {
 
         doNothing().when(optionsService).deleteOption(eq(userId), eq(surveyId), eq(questionId), eq(optionId));
 
-        ResultActions actions = mockMvc.perform(delete("/api/survey/{surveyId}/question/{questionId}/option/{optionId}", surveyId, questionId, optionId));
+        ResultActions actions = mockMvc.perform(delete("/api/survey/{surveyId}/question/{questionId}/option/{optionId}", surveyId, questionId, optionId)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer {jwt_token}"));
 
         verify(optionsService, times(1))
                 .deleteOption(eq(userId), eq(surveyId), eq(questionId), eq(optionId));
 
         actions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.data").isEmpty());
+                .andExpect(jsonPath("$.data").isEmpty())
+                .andDo(document("delete-option",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("JWT 인증 토큰 (Bearer + 토큰 값)")
+                                        .attributes(key("format").value("Bearer {jwt_token"))
+                        ),
+                        pathParameters(
+                                parameterWithName("surveyId").description("설문 ID"),
+                                parameterWithName("questionId").description("질문 ID"),
+                                parameterWithName("optionId").description("선택지 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("요청 성공 여부"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("요청 결과 메시지"),
+                                fieldWithPath("timestamp").type(JsonFieldType.STRING).description("타임스탬프"),
+                                fieldWithPath("data").type(JsonFieldType.NULL).description("응답 Data")
+                        )
+                ))
+        ;
     }
 }
