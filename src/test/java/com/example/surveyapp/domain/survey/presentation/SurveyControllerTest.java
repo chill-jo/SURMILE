e9821c65b2a176c1780b33f7b35d2f53aeafa4f7
gsemily <gsemily22@gmail.com>
2025-08-18 03:14:53 +0900

    fix: merge 문제 해결

--- src/test/java/com/example/surveyapp/domain/survey/presentation/SurveyControllerTest.java
+++ src/test/java/com/example/surveyapp/domain/survey/presentation/SurveyControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
