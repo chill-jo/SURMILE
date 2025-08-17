918a66721a333f4be6cc166efff310c1b907e46b
gsemily <gsemily22@gmail.com>
2025-08-18 00:05:17 +0900

    feat: 챗봇 예외처리 추가
    - AiException 추가
    - AiErrorCode 추가
    - requestDTO에 NotBlank 옵션 추가

--- src/main/java/com/example/surveyapp/domain/ai/chat/presentation/dto/ChatRequestDto.java
+++ src/main/java/com/example/surveyapp/domain/ai/chat/presentation/dto/ChatRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.surveyapp.domain.ai.chat.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ChatRequestDto {
+    @NotBlank
     private String question;
 }
--- src/main/java/com/example/surveyapp/domain/ai/chat/presentation/dto/IndexRequestDto.java
+++ src/main/java/com/example/surveyapp/domain/ai/chat/presentation/dto/IndexRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.surveyapp.domain.ai.chat.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class IndexRequestDto {
+    @NotBlank
     private String document;
 }
