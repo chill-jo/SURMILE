bde9150f5b8ad666dfc9efee4f9c595ef3837263
kimminsung <shiwing15@naver.com>
2025-08-18 01:28:48 +0900

    refactor: eventhandler로 패키지 이름 변경

---
+++ src/main/java/com/example/surveyapp/domain/point/application/eventhandler/OrderPointEventHandler.java
@@ -0,0 +1,26 @@
+package com.example.surveyapp.domain.point.application.eventhandler;
+
+import com.example.surveyapp.domain.order.domain.event.OrderCreateEvent;
+import com.example.surveyapp.domain.point.application.PointEarnRedeemService;
+import com.example.surveyapp.domain.point.domain.model.entity.vo.PointBalance;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class OrderPointEventHandler {
+
+    private final PointEarnRedeemService pointEarnRedeemService;
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleOrderCreateEvent(OrderCreateEvent event) {
+        pointEarnRedeemService.decreasePoint(
+                event.getUserId(),
+                PointBalance.of(event.getTotalAmount()),
+                event.getOrderId());
+    }
+}
