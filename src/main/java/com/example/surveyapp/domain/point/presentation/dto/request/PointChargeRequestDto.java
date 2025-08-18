bf9cd0f46b5b32626b9199d6f213ed2f3c0795e5
handokei <ehgks994@naver.com>
2025-08-18 13:07:25 +0900

    point event 리펙토링 Issues/332 (#334)
    * refactor: JSON 직렬화/역직렬화를 위한 수정
    
    * refactor: 이벤트 발행 위치 수정
    
    * feat: 이벤트 성공 이벤트 실패 이벤트 생성
    
    * refactor: PreAuthorize 제거
    
    * refactor: 패지키 수정

--- src/main/java/com/example/surveyapp/domain/point/presentation/dto/request/PointChargeRequestDto.java
+++ src/main/java/com/example/surveyapp/domain/point/presentation/dto/request/PointChargeRequestDto.java
@@ -2,16 +2,17 @@ package com.example.surveyapp.domain.point.presentation.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PointChargeRequestDto {
 
     @NotNull
     @Min(value=5000, message="최소 충전 금액은 5,000원 이상이어야 합니다.")
-    private final Long price;
+    private Long price;
 
-    public PointChargeRequestDto(Long price){
-        this.price = price;
-    }
 }
