3d425251388fdfecdb7cfb05997d25246c63e3f6
handokei <ehgks994@naver.com>
2025-08-18 12:24:02 +0900

    refactor: try{} catch 문 활용으로 성공 및 금액 부족으로 인한 실패, 환경요인으로 인한 실패로 분리

--- src/main/java/com/example/surveyapp/domain/point/application/eventhandler/OrderPointEventHandler.java
+++ src/main/java/com/example/surveyapp/domain/point/application/eventhandler/OrderPointEventHandler.java
@@ -2,8 +2,12 @@ package com.example.surveyapp.domain.point.application.eventhandler;
 
 import com.example.surveyapp.domain.order.domain.event.OrderCreateEvent;
 import com.example.surveyapp.domain.point.application.PointEarnRedeemService;
+import com.example.surveyapp.domain.point.domain.model.entity.event.PointRedeemFailedEvent;
 import com.example.surveyapp.domain.point.domain.model.entity.vo.PointBalance;
+import com.example.surveyapp.domain.point.exception.PointException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -11,16 +15,38 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OrderPointEventHandler {
 
     private final PointEarnRedeemService pointEarnRedeemService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleOrderCreateEvent(OrderCreateEvent event) {
-        pointEarnRedeemService.decreasePoint(
-                event.getUserId(),
-                PointBalance.of(event.getTotalAmount()),
-                event.getOrderId());
+        try {
+            pointEarnRedeemService.decreasePoint(
+                    event.getUserId(),
+                    PointBalance.of(event.getTotalAmount()),
+                    event.getOrderId());
+
+        }
+        catch (PointException e) {
+            eventPublisher.publishEvent(new PointRedeemFailedEvent(event.getOrderId(),
+                    event.getUserId()));
+            log.info("포인트 차감 실패(PointException) – orderId={}, userId={}, reason={}",
+                    event.getOrderId(), event.getUserId(), e.getMessage(), e);
+                    log.info("포인트 차감 메서드 금액 실패 후 이밴트 발행");
+
+        }
+        catch (Exception e){
+            eventPublisher.publishEvent(new PointRedeemFailedEvent(event.getOrderId(),
+                    event.getUserId()));
+            log.info("포인트 차감 실패(기타) – orderId={}, userId={}", event.getOrderId(), event.getUserId(), e);
+
+            log.info("포인트 차감 메서드 실패 후 이밴트 발행");
+//메서드 실패 이유-> 인터넷장애, 메서드 실패, 트랜잭션 실패
+        }
+
     }
 }
