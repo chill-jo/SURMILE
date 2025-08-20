e68ccb04514efdd080f50d58f90a0f24a8391746
HyungWo-o <dbwotns153@naver.com>
2025-08-20 19:01:46 +0900

    fix: 로그인 로그아웃 로그인연장 Redis TestContainer TC 수정

--- src/test/java/com/example/surveyapp/global/RedisTest.java
+++ src/test/java/com/example/surveyapp/global/RedisTest.java
@@ -3,13 +3,14 @@ package com.example.surveyapp.global;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.surveyapp.domain.user.domain.model.UserRoleEnum;
+import com.example.surveyapp.domain.user.presentation.dto.RegisterRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 import com.example.surveyapp.domain.user.application.UserService;
 import com.example.surveyapp.domain.user.presentation.dto.LoginRequestDto;
@@ -29,12 +30,13 @@ public class RedisTest {
 	private UserService userService;
 
 	@Test
-	@Sql("classpath:test-auth.sql")
 	@DisplayName("기능: 로그인을 성공한다")
 	public void success_login() {
 
 		// Given
-		LoginRequestDto requestDto = new LoginRequestDto("test@example.com", "password123!");
+        RegisterRequestDto registerRequestDto = new RegisterRequestDto("test1@example.com", "Password!234", "Password!234", "test1", "test1", UserRoleEnum.SURVEYOR);
+        userService.register(registerRequestDto);
+		LoginRequestDto requestDto = new LoginRequestDto("test1@example.com", "Password!234");
 
 		// When
 		LoginResponseDto result = assertDoesNotThrow(() -> userService.login(requestDto));
@@ -47,23 +49,26 @@ public class RedisTest {
 	@DisplayName("기능: 로그아웃을 성공한다")
 	public void success_logout() {
 		//Given
-		String accessToken = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyIiwicm9sZSI6IlNVUlZFWU9SIiwiaWF0IjoxNzU1NTE4MzM5LCJleHAiOjE3NTU1MjAxMzl9.73bn6HxpToCns02lstvHmKUUntyY3OLRKuxkZCKT9b7y6jPnVqdUHZPjPZ4evu7p";
+		String accessToken = "Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyIiwicm9sZSI6IlNVUlZFWU9SIiwiaWF0IjoxNzU1NjgyMzQwLCJleHAiOjE3NTU2ODQxNDB9.cz32waE8lqiQQ7sK6igrNVpZzyNZ4_1iVm_OACeWIOrTsAsBrTI9N9U_t6u2eEg6";
 
 		//When
 		userService.logout(accessToken);
 
 		//Then
 		String actual = redisTemplateFacade.read("accessToken:2", String.class);
-		assertThat(actual).isEqualTo(accessToken);
+		assertThat("Bearer " + actual).isEqualTo(accessToken);
 	}
 
 	@Test
-	//    @Sql("classpath:test-auth.sql")
 	@DisplayName("기능: 리프레시 토큰을 재발급 받는다")
 	public void success_refreshToken() {
 
 		//Given
-		String refreshToken = "refresh_token";
+        RegisterRequestDto registerRequestDto = new RegisterRequestDto("test2@example.com", "Password!234", "Password!234", "test2", "test2", UserRoleEnum.SURVEYOR);
+        userService.register(registerRequestDto);
+        LoginRequestDto requestDto = new LoginRequestDto("test2@example.com", "Password!234");
+        LoginResponseDto loginResponseDto = userService.login(requestDto);
+		String refreshToken = loginResponseDto.getRefreshToken();
 
 		//when
 		LoginResponseDto result = userService.refresh(refreshToken);
