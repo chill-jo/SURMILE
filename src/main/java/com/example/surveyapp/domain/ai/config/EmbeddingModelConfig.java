7930217cdbf6c65b4fe6e8b8d54ad1f4a14ef086
gsemily <120404242+gsemily@users.noreply.github.com>
2025-08-20 15:25:01 +0900

    Issues/341 벡터스토어 모델 변경 (#342)
    * refactor: ai vectorstore 관련 gradle 의존성 변경
    
    * refactor: vectorstore 설정 삭제
    
    * refactor: EmbeddingModelConfig 벡터스토어 모델 변경
    
    * chore: ChatPrompt, AiModerationPrompt 내용 수정
    
    * chore: line

--- src/main/java/com/example/surveyapp/domain/ai/config/EmbeddingModelConfig.java
+++ src/main/java/com/example/surveyapp/domain/ai/config/EmbeddingModelConfig.java
@@ -1,25 +1,15 @@
 package com.example.surveyapp.domain.ai.config;
 
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class EmbeddingModelConfig {
     @Bean
-    public JedisPooled jedisPooled() {
-        return new JedisPooled("localhost", 6379);
-    }
-
-    @Bean
-    public VectorStore vectorStore(JedisPooled jedis, EmbeddingModel embeddingModel) {
-        return RedisVectorStore.builder(jedis, embeddingModel)
-                .indexName("spring-ai-index")
-                .prefix("embedding:")
-                .initializeSchema(true)
-                .build();
+    VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
     }
 }
