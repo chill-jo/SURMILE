95c9c618ddfdde850454696e0845eadb994f8837
handokei <ehgks994@naver.com>
2025-08-22 21:46:02 +0900

    feat: Event 발행, 실패 시 재시도 로직 구성, 최대 횟수 5회 제한.

---
+++ src/main/java/com/example/surveyapp/domain/payment/application/PaymentOutboxScheduler.java
@@ -0,0 +1,46 @@
+package com.example.surveyapp.domain.payment.application;
+
+import com.example.surveyapp.domain.payment.domain.event.PointChargeEvent;
+import com.example.surveyapp.domain.payment.domain.model.entity.PaymentOutbox;
+import com.example.surveyapp.domain.payment.domain.model.enums.PaymentOutboxEnum;
+import com.example.surveyapp.domain.payment.domain.repository.PaymentOutboxRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentOutboxScheduler {
+
+    private final ObjectMapper objectMapper;
+    private final PaymentOutboxRepository paymentOutboxRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    private static final int MAX_RETRY = 5;
+
+    @Scheduled(fixedDelay = 10_000)
+    @Transactional
+    public void publishOutboxEvents() throws Exception {
+        List<PaymentOutbox> unpublished = paymentOutboxRepository.findByStatusAndPublished(PaymentOutboxEnum.READY, false);
+
+        for (PaymentOutbox paymentOutbox : unpublished) {
+            try {
+                eventPublisher.publishEvent(objectMapper.readValue(paymentOutbox.getPayload(), PointChargeEvent.class));
+                paymentOutbox.markPublished();
+
+            }catch (Exception e) {
+
+                paymentOutbox.markFailed(MAX_RETRY);
+            }
+            paymentOutboxRepository.save(paymentOutbox);
+        }
+
+    }
+}
