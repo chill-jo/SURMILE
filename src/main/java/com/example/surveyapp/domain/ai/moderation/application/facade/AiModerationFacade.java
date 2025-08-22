fb7a4ebac96699940a56d73e785a7fa9aa665376
gsemily <gsemily22@gmail.com>
2025-08-22 22:22:48 +0900

    "Refactor: AiModerationFacade 메서드 수정"

--- src/main/java/com/example/surveyapp/domain/ai/moderation/application/facade/AiModerationFacade.java
+++ src/main/java/com/example/surveyapp/domain/ai/moderation/application/facade/AiModerationFacade.java
@@ -3,10 +3,10 @@ package com.example.surveyapp.domain.ai.moderation.application.facade;
 import com.example.surveyapp.domain.ai.moderation.domain.model.vo.AiModerationResult;
 
 public interface AiModerationFacade {
-    AiModerationResult checkNicknameModeration(String nickname);
-    AiModerationResult checkTitleModeration(String title);
-    AiModerationResult checkDescriptionModeration(String description);
-    AiModerationResult checkQuestionModeration(String question);
-    AiModerationResult checkOptionsModeration(String options);
-    AiModerationResult checkTextAnswerModeration(String content);
+    AiModerationResult checkNicknameModeration(Long userId, String Email, String nickname);
+    AiModerationResult checkTitleModeration(Long userId, String title);
+    AiModerationResult checkDescriptionModeration(Long userId, String description);
+    AiModerationResult checkQuestionModeration(Long userId, String question);
+    AiModerationResult checkOptionsModeration(Long userId, String options);
+    AiModerationResult checkTextAnswerModeration(Long userId, String content);
 }
--- src/main/java/com/example/surveyapp/domain/ai/moderation/application/facade/AiModerationFacadeImpl.java
+++ src/main/java/com/example/surveyapp/domain/ai/moderation/application/facade/AiModerationFacadeImpl.java
@@ -3,6 +3,7 @@ package com.example.surveyapp.domain.ai.moderation.application.facade;
 import com.example.surveyapp.domain.ai.moderation.application.AiModerationService;
 import com.example.surveyapp.domain.ai.moderation.domain.model.enums.AiModerationTargetType;
 import com.example.surveyapp.domain.ai.moderation.domain.model.vo.AiModerationResult;
+import com.example.surveyapp.global.reader.UserReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,34 +11,35 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AiModerationFacadeImpl implements AiModerationFacade {
     private final AiModerationService aiModerationService;
+    private final UserReader userReader;
 
     @Override
-    public AiModerationResult checkNicknameModeration(String nickname) {
-        return aiModerationService.moderate(AiModerationTargetType.USER_NICKNAME, nickname);
+    public AiModerationResult checkNicknameModeration(Long userId, String email, String nickname) {
+        return aiModerationService.moderate(userId, email, AiModerationTargetType.USER_NICKNAME, nickname);
     }
 
     @Override
-    public AiModerationResult checkTitleModeration(String title) {
-        return aiModerationService.moderate(AiModerationTargetType.SURVEY_TITLE, title);
+    public AiModerationResult checkTitleModeration(Long userId, String title) {
+        return aiModerationService.moderate(userId, userReader.emailById(userId), AiModerationTargetType.SURVEY_TITLE, title);
     }
 
     @Override
-    public AiModerationResult checkDescriptionModeration(String description){
-        return aiModerationService.moderate(AiModerationTargetType.SURVEY_DESCRIPTION, description);
+    public AiModerationResult checkDescriptionModeration(Long userId, String description){
+        return aiModerationService.moderate(userId, userReader.emailById(userId), AiModerationTargetType.SURVEY_DESCRIPTION, description);
     }
 
     @Override
-    public AiModerationResult checkQuestionModeration(String question) {
-        return aiModerationService.moderate(AiModerationTargetType.SURVEY_QUESTION, question);
+    public AiModerationResult checkQuestionModeration(Long userId, String question) {
+        return aiModerationService.moderate(userId, userReader.emailById(userId), AiModerationTargetType.SURVEY_QUESTION, question);
     }
 
     @Override
-    public AiModerationResult checkOptionsModeration(String options) {
-        return aiModerationService.moderate(AiModerationTargetType.SURVEY_OPTIONS, options);
+    public AiModerationResult checkOptionsModeration(Long userId, String options) {
+        return aiModerationService.moderate(userId, userReader.emailById(userId), AiModerationTargetType.SURVEY_OPTIONS, options);
     }
 
     @Override
-    public AiModerationResult checkTextAnswerModeration(String answer) {
-        return aiModerationService.moderate(AiModerationTargetType.SURVEY_TEXT_ANSWER, answer);
+    public AiModerationResult checkTextAnswerModeration(Long userId, String answer) {
+        return aiModerationService.moderate(userId, userReader.emailById(userId), AiModerationTargetType.SURVEY_TEXT_ANSWER, answer);
     }
 }
