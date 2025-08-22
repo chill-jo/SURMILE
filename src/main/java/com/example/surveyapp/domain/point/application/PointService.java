fc1c3c2528ea6bb54df2c017605ae1f13acb71f9
kimminsung <shiwing15@naver.com>
2025-08-22 10:02:00 +0900

    refactor: 로그 삭제, 이벤트 이름 변경 반영, feat: 출제자 포인트 충전 기능 PointService -> PointEarnRedeemService로 이동, SAGA 패턴 적용

--- src/main/java/com/example/surveyapp/domain/point/application/PointService.java
+++ src/main/java/com/example/surveyapp/domain/point/application/PointService.java
@@ -2,19 +2,13 @@ package com.example.surveyapp.domain.point.application;
 
 import com.example.surveyapp.domain.point.exception.PointErrorCode;
 import com.example.surveyapp.domain.point.exception.PointException;
-import com.example.surveyapp.domain.point.presentation.dto.request.PointChargeRequestDto;
 import com.example.surveyapp.domain.point.presentation.dto.response.PointHistoryResponseDto;
 import com.example.surveyapp.domain.point.domain.model.entity.*;
-import com.example.surveyapp.domain.point.domain.model.entity.vo.Money;
-import com.example.surveyapp.domain.point.domain.model.entity.vo.PointBalance;
-import com.example.surveyapp.domain.point.domain.model.enums.*;
-import com.example.surveyapp.domain.point.domain.repository.PaymentRepository;
 import com.example.surveyapp.domain.point.domain.repository.PointHistoryRepository;
 import com.example.surveyapp.domain.point.domain.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,10 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PointService {
 
-    private final MoneyToPointService moneyToPointService;
     private final PointRepository pointRepository;
     private final PointHistoryRepository pointHistoryRepository;
-    private final PaymentRepository paymentRepository;
 
     @Transactional
     public void createPointWallet(Long userId){
@@ -35,46 +27,6 @@ public class PointService {
             PointWallet pointWallet = PointWallet.of(userId);
             pointRepository.save(pointWallet);
         }
-    }
-
-    // 충전
-    @Transactional
-    public void charge(Long userId, PointChargeRequestDto dto){
-        //요청받은 금액
-
-        Money amount = Money.krw(dto.getPrice());
-
-        PointBalance chargePoint = moneyToPointService.convert(amount);
-
-        PointWallet pointWallet = getPoint(userId);
-
-        //충전 전 금액
-        PointBalance currentBalance = pointWallet.getPointBalance();
-
-        //포인트 충전. (dirty checking)
-        pointWallet.pointCharge(chargePoint);
-
-        Payment payment = Payment.of(
-                amount,
-                PointStatus.DONE,
-                Method.KAKAO_PAY,
-                TargetType.POINT_CHARGE
-        );
-        paymentRepository.save(payment);
-
-        PointHistory history = PointHistory.of(
-                currentBalance,
-                chargePoint,
-                pointWallet.getPointBalance(),
-                PointType.CHARGE,
-                Target.PAYMENTS,
-                payment.getId(),
-                "포인트 충전",
-                userId,
-                pointWallet
-        );
-
-        pointHistoryRepository.save(history);
     }
 
     @Transactional(readOnly = true)
