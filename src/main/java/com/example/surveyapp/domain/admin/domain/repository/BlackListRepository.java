2cbf3558ae63604a88622103d97e76b5954cca83
HyungWo-o <dbwotns153@naver.com>
2025-08-23 00:00:01 +0900

    fix: 블랙리스트 버그 수정

--- src/main/java/com/example/surveyapp/domain/admin/domain/repository/BlackListRepository.java
+++ src/main/java/com/example/surveyapp/domain/admin/domain/repository/BlackListRepository.java
@@ -2,9 +2,11 @@ package com.example.surveyapp.domain.admin.domain.repository;
 
 import com.example.surveyapp.domain.admin.domain.model.BlackList;
 
+import java.util.Optional;
+
 public interface BlackListRepository {
 
-    BlackList findByUserId(Long userId);
+    Optional<BlackList> findByUserId(Long userId);
     boolean existsByUserId(Long userId);
 
     BlackList save(BlackList blackList);
