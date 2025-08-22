e7647d6cee89a7fb78d43d03fff09852cbac9dac
handokei <ehgks994@naver.com>
2025-08-22 10:18:40 +0900

    refactor: Point infrastructure 구성

--- src/main/java/com/example/surveyapp/domain/point/infrastructure/PointJpaRepository.java
+++ src/main/java/com/example/surveyapp/domain/point/infrastructure/PointJpaRepository.java
@@ -1,14 +1,11 @@
-package com.example.surveyapp.domain.point.domain.repository;
+package com.example.surveyapp.domain.point.infrastructure;
 
 import com.example.surveyapp.domain.point.domain.model.entity.PointWallet;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
-public interface PointRepository {
-//    @Lock(LockModeType.PESSIMISTIC_WRITE)
+public interface PointJpaRepository extends JpaRepository<PointWallet, Long> {
     Optional<PointWallet> findByUserId(Long userId);
     Boolean existsByUserId(Long userId);
 
