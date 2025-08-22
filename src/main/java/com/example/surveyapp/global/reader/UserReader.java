c85f2e9708b7a6d34d2ff9c498be2ae9049f00f0
gsemily <gsemily22@gmail.com>
2025-08-22 22:23:28 +0900

    "Refactor: UserReader emailbyId 메서드 추가"

--- src/main/java/com/example/surveyapp/global/reader/UserReader.java
+++ src/main/java/com/example/surveyapp/global/reader/UserReader.java
@@ -5,6 +5,7 @@ import com.example.surveyapp.domain.user.domain.model.UserRoleEnum;
 public interface UserReader {
     void validateUserIdOrThrow(Long userId);
     String usernameById(Long userId);
+    String emailById(Long userId);
     boolean validateUserRoleToAdmin(Long userId);
     boolean validateUserRoleToSurveyee(Long userId);
     boolean validateUserRoleToSurveyor(Long userId);
