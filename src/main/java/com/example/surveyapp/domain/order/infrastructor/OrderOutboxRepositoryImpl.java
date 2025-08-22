24d912b03171582b74f5ad184f8333e90db28bc8
handokei <ehgks994@naver.com>
2025-08-22 19:45:29 +0900

    feat: Repository와 Interface 구현체 구현

---
+++ src/main/java/com/example/surveyapp/domain/order/infrastructor/OrderOutboxRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.example.surveyapp.domain.order.infrastructor;
+
+
+import com.example.surveyapp.domain.order.domain.model.OrderOutbox;
+import com.example.surveyapp.domain.order.domain.model.vo.OrderOutBoxEnum;
+import com.example.surveyapp.domain.order.domain.repository.OrderOutboxRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderOutboxRepositoryImpl implements OrderOutboxRepository {
+
+    private final OrderOutboxJpaRepository orderOutboxJpaRepository;
+
+    @Override
+    public OrderOutbox save(OrderOutbox orderOutbox) {
+        return orderOutboxJpaRepository.save(orderOutbox);
+    }
+
+    @Override
+    public List<OrderOutbox> findByStatusAndPublished(OrderOutBoxEnum status, boolean published) {
+        return orderOutboxJpaRepository.findByStatusAndPublished(status,published);
+    }
+}
