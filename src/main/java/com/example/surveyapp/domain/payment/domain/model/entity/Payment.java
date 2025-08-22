20f98fe7410a27a178927a4cd6b7b2af9a96d959
kimminsung <shiwing15@naver.com>
2025-08-22 10:15:35 +0900

    feat: Payment 도메인 분리로 Entity, Enum, Vo 패키지 변경

---
+++ src/main/java/com/example/surveyapp/domain/payment/domain/model/entity/Payment.java
@@ -0,0 +1,67 @@
+package com.example.surveyapp.domain.payment.domain.model.entity;
+
+import com.example.surveyapp.domain.payment.domain.model.vo.Money;
+import com.example.surveyapp.domain.payment.domain.model.enums.Method;
+import com.example.surveyapp.domain.payment.domain.model.enums.TargetType;
+import com.example.surveyapp.domain.payment.domain.model.enums.PointStatus;
+import com.example.surveyapp.global.config.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Payment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Money amount;
+
+    @Column(unique = true, nullable = false)
+    private UUID paymentKey;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PointStatus pointStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Method method;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TargetType targetType;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Payment(Money amount, PointStatus pointStatus, Method method, TargetType targetType) {
+        this.amount = amount;
+        this.paymentKey = UUID.randomUUID();
+        this.pointStatus = pointStatus;
+        this.method = method;
+        this.targetType = targetType;
+    }
+
+    public static Payment of(Money amount, PointStatus pointStatus, Method method, TargetType targetType){
+        return Payment.builder()
+                .amount(amount)
+                .pointStatus(pointStatus)
+                .method(method)
+                .targetType(targetType)
+                .build();
+    }
+
+    public void confirm(){
+        this.pointStatus = PointStatus.CONFIRM;
+    }
+
+    public void fail(){
+        this.pointStatus = PointStatus.FAILED;
+    }
+}
