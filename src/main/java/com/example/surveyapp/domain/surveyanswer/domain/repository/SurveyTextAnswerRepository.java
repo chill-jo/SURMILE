8b390bc29e25fc063fac70b131dd400a2bdcc0fa
handokei <ehgks994@naver.com>
2025-08-22 22:01:35 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/example/surveyapp/domain/surveyanswer/domain/repository/SurveyTextAnswerRepository.java
+++ src/main/java/com/example/surveyapp/domain/surveyanswer/domain/repository/SurveyTextAnswerRepository.java
@@ -1,6 +1,6 @@
 package com.example.surveyapp.domain.surveyanswer.domain.repository;
 
-import com.example.surveyapp.domain.surveyanswer.domain.model.SurveyTextAnswer;
+import com.example.surveyapp.domain.surveyanswer.domain.model.entity.SurveyTextAnswer;
 
 public interface SurveyTextAnswerRepository {
     SurveyTextAnswer save(SurveyTextAnswer surveyTextAnswer);
