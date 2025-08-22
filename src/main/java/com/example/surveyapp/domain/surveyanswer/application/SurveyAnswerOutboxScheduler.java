d268513a2df44690aae0524b4303a927b2eb84df
handokei <ehgks994@naver.com>
2025-08-22 22:03:23 +0900

    feat: Event 발행, 실패 시 재시도 로직 구성, 최대 횟수 5회 제한.

---
+++ src/main/java/com/example/surveyapp/domain/surveyanswer/application/SurveyAnswerOutboxScheduler.java
@@ -0,0 +1,43 @@
+package com.example.surveyapp.domain.surveyanswer.application;
+
+import com.example.surveyapp.domain.surveyanswer.domain.event.SurveyAnswerEvent;
+import com.example.surveyapp.domain.surveyanswer.domain.model.entity.AnserEnum;
+import com.example.surveyapp.domain.surveyanswer.domain.model.entity.SurveyAnswerOutbox;
+import com.example.surveyapp.domain.surveyanswer.domain.repository.SurveyAnswerOutboxRepository;
+import com.example.surveyapp.domain.surveyanswer.exception.AnswerException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SurveyAnswerOutboxScheduler {
+
+    private final ObjectMapper objectMapper;
+    private final SurveyAnswerOutboxRepository surveyAnswerOutboxRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    private static final int MAX_RETRY = 5;
+
+    @Scheduled(fixedDelay = 10_000)
+    @Transactional
+    public void publishOutboxEvents() throws Exception {
+    List<SurveyAnswerOutbox> unpublished = surveyAnswerOutboxRepository.findByStatusAndPublished(AnserEnum.FAILED, false);
+
+    for (SurveyAnswerOutbox answerOutbox : unpublished) {
+        try {
+            eventPublisher.publishEvent(objectMapper.readValue(answerOutbox.getPayload(),SurveyAnswerEvent.class));
+            answerOutbox.markPublished();
+        }catch (AnswerException e) {
+            answerOutbox.markFailed(MAX_RETRY);
+        }
+        surveyAnswerOutboxRepository.save(answerOutbox);
+    }
+
+    }
+}
