0c50986d0012e17aa27ff29ccaa0d71b32859b7a
handokei <ehgks994@naver.com>
2025-08-22 21:45:38 +0900

    refactor: Event 발행 위치 변경, Event는 Outbox로 관리하기 위해 OutboxEvent 생성 및 저장

--- src/main/java/com/example/surveyapp/domain/payment/application/PaymentService.java
+++ src/main/java/com/example/surveyapp/domain/payment/application/PaymentService.java
@@ -4,19 +4,25 @@ import com.example.surveyapp.domain.payment.domain.event.PointChargeEvent;
 import com.example.surveyapp.domain.payment.domain.model.entity.Payment;
 import com.example.surveyapp.domain.payment.domain.model.enums.*;
 import com.example.surveyapp.domain.payment.domain.model.vo.Money;
+import com.example.surveyapp.domain.payment.domain.repository.PaymentOutboxRepository;
 import com.example.surveyapp.domain.payment.domain.repository.PaymentRepository;
+import com.example.surveyapp.domain.payment.exception.PaymentErrorCode;
+import com.example.surveyapp.domain.payment.exception.PaymentException;
 import com.example.surveyapp.domain.payment.presentation.dto.request.PointChargeRequestDto;
 import com.example.surveyapp.domain.payment.presentation.dto.response.PointChargeResponseDto;
+import com.example.surveyapp.domain.payment.domain.model.entity.PaymentOutbox;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
-    private final ApplicationEventPublisher eventPublisher;
+    private final PaymentOutboxRepository paymentOutboxRepository;
+    private final ObjectMapper objectMapper;
 
     @Transactional
     public PointChargeResponseDto charge(Long userId, PointChargeRequestDto requestDto){
@@ -32,12 +38,24 @@ public class PaymentService {
 
         paymentRepository.save(payment);
 
-        eventPublisher.publishEvent(
-                new PointChargeEvent(
-                        userId,
-                        payment.getId(),
-                        requestDto.getPrice()
-                ));
+        try{
+            PointChargeEvent event = new PointChargeEvent(
+                    userId,
+                    payment.getId(),
+                    requestDto.getPrice()
+            );
+
+            String payload = objectMapper.writeValueAsString(event);
+
+            PaymentOutbox paymentOutbox = PaymentOutbox.of(
+                    "Payment",
+                    payment.getId(),
+                    payload);
+
+            paymentOutboxRepository.save(paymentOutbox);
+        } catch (JsonProcessingException e) {
+            throw new PaymentException(PaymentErrorCode.CANNOT_CONVERT_PAYLOAD);
+        }
 
         return new PointChargeResponseDto(
                 payment.getId(),
