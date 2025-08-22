0b919273bffa31213676d001dc2ef49da60b48b9
kimminsung <shiwing15@naver.com>
2025-08-22 09:59:31 +0900

    refactor: SurveyPointRedeemEvent 이벤트 이름 변경

--- src/main/java/com/example/surveyapp/domain/survey/application/eventhandler/SurveyEventHandler.java
+++ src/main/java/com/example/surveyapp/domain/survey/application/eventhandler/SurveyEventHandler.java
@@ -1,7 +1,7 @@
 package com.example.surveyapp.domain.survey.application.eventhandler;
 
-import com.example.surveyapp.domain.point.domain.event.PointRedeemFailEvent;
-import com.example.surveyapp.domain.point.domain.event.PointRedeemSuccessEvent;
+import com.example.surveyapp.domain.point.domain.event.SurveyPointRedeemFailedEvent;
+import com.example.surveyapp.domain.point.domain.event.SurveyPointRedeemSucceededEvent;
 import com.example.surveyapp.domain.survey.application.SurveyQueryService;
 import com.example.surveyapp.domain.survey.domain.model.entity.Survey;
 import com.example.surveyapp.domain.survey.domain.model.enums.SurveyStatus;
@@ -33,7 +33,7 @@ public class SurveyEventHandler {
     @Async
     @Transactional
     @EventListener
-    public void handlePointRedeemEvent(PointRedeemFailEvent event){
+    public void handlePointRedeemFailEvent(SurveyPointRedeemFailedEvent event){
         log.info("포인트 차감 실패 이벤트 받음");
         Survey survey = surveyQueryService.findSurvey(event.getSurveyId());
 
@@ -45,7 +45,7 @@ public class SurveyEventHandler {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Async
     @Transactional
-    public void handlePointRedeemSuccessEvent(PointRedeemSuccessEvent event){
+    public void handlePointRedeemSuccessEvent(SurveyPointRedeemSucceededEvent event){
         log.info("포인트 차감 성공 이벤트 받음");
         Survey survey = surveyQueryService.findSurvey(event.getSurveyId());
 
