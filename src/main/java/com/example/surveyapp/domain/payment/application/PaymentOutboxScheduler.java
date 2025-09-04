1d02d31c1df5c58c31193c1bc4c4632419d388b6
handokei <ehgks994@naver.com>
2025-09-04 17:22:05 +0900

    refactor: 불필요한 import정문 삭제

--- src/main/java/com/example/surveyapp/domain/payment/application/PaymentOutboxScheduler.java
+++ src/main/java/com/example/surveyapp/domain/payment/application/PaymentOutboxScheduler.java
@@ -7,7 +7,6 @@ import com.example.surveyapp.domain.payment.domain.repository.PaymentOutboxRepos
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
